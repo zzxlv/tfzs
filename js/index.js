ee04050f7241a5c132680dfb110881ac3a87bcec
zzxlv <15167194550@163.com>
2019-05-28 15:23:09 +0800

    zz

--- js/index.js
+++ js/index.js
@@ -1,12 +1,4 @@
 $(document).ready(function(){
-	/* funcReadImgInfo();*/
-	
-	for(var i=0;i<$("img").length;i++){
-					var src=$("img").eq(i).attr("src");
-					$("img").eq(i).attr("data-url",src);
-					$("img").eq(i).attr("src","");
-				}
-				$("img").addClass("scrollLoading");
 			   //实现图片慢慢浮现出来的效果 
 				$("img").on('load',function(){
 			     //图片默认隐藏  
